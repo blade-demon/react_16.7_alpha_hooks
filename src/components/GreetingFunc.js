cdd15bcaefbdb5db77c479d955b9b40b8d616736
徐紫微 <xuziwei89@gmail.com>
2018-11-27 22:18:00 +0800

    extract code to generate custom hooks

--- src/components/GreetingFunc.js
+++ src/components/GreetingFunc.js
@@ -2,32 +2,13 @@ import React, { useState, useEffect, useContext } from "react";
 import { ThemeContext, LocaleContext } from "../context";
 
 export default function GreetingFunc() {
-  const [name, setName] = useState("Henry");
-  const [email, setEmail] = useState("xuziwei89@gmail.com");
+  const name = useFormInput("Henry");
+  const email = useFormInput("xuziwei89@gmail.com");
+
   const theme = useContext(ThemeContext);
   const language = useContext(LocaleContext);
-
-  useEffect(() => {
-    document.title = name + "   " + email;
-  });
-
-  // register event to browser event
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    // register evenrtListener
-    window.addEventListener("resize", handleResize);
-    // if return function，it will clean eventListener
-    return () => window.removeEventListener("resize", handleResize);
-  });
-
-  function handleNameChange(e) {
-    setName(e.target.value);
-  }
-
-  function handleEmailChange(e) {
-    setEmail(e.target.value);
-  }
+  const width = useWindowWidth();
+  useDocumentTitle(name.value + "   " + email.value);
 
   return (
     <React.Fragment>
@@ -37,14 +18,15 @@ export default function GreetingFunc() {
           <div>
             <label>Name </label>
             <br />
-            <input type="text" value={name} onChange={handleNameChange} />
+
+            <input type="text" {...name} />
           </div>
         </section>
         <section>
           <div>
             <label>Email</label>
             <br />
-            <input type="text" value={email} onChange={handleEmailChange} />
+            <input type="text" {...email} />
           </div>
         </section>
         <section>
@@ -64,4 +46,38 @@ export default function GreetingFunc() {
       </div>
     </React.Fragment>
   );
+}
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  function handleChange(e) {
+    setValue(e.target.value);
+  }
+
+  return {
+    value,
+    onChange: handleChange
+  };
+}
+
+function useDocumentTitle(title) {
+  useEffect(() => {
+    document.title = title;
+  });
+}
+
+// Custom hooks, extracted from component.
+function useWindowWidth() {
+  // register event to browser event
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    // register evenrtListener
+    window.addEventListener("resize", handleResize);
+    // if return function，it will clean eventListener
+    return () => window.removeEventListener("resize", handleResize);
+  });
+
+  return width;
 }
